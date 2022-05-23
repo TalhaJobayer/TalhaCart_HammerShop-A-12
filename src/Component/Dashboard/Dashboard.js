86b73d0943d70f0f9ce8924debf8f81a5e0576f7
Talha Jobayer <talhajobayer@gmail.com>
2022-05-23 19:24:13 +0600

    dashboard added

--- src/Component/Dashboard/Dashboard.js
+++ src/Component/Dashboard/Dashboard.js
@@ -2,9 +2,25 @@ import React from 'react';
 
 const Dashboard = () => {
     return (
-        <div>
-            
-        </div>
+        <div class="drawer drawer-mobile">
+  <input id="dashboard-drawer" type="checkbox" class="drawer-toggle" />
+  <div class="drawer-content flex flex-col items-center justify-center">
+    {/* <!-- Page content here --> */}
+    <label for="dashboard-drawer" class="btn btn-primary drawer-button lg:hidden">Open drawer</label>
+  
+  </div> 
+  <div class="drawer-side">
+    <label for="dashboard-drawer" class="drawer-overlay"></label> 
+    <ul class="menu p-4 overflow-y-auto w-80 bg-base-100 text-base-content">
+      {/* <!-- Sidebar content here --> */}
+      <li><a>Sidebar Item 1</a></li>
+      <li><a>Sidebar Item 2</a></li>
+    </ul>
+  
+  </div>
+  
+</div>
+
     );
 };
 
